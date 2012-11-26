f940ee199670b3bcc9048a8e6314f284e0a506b0
franciscofranco <franciscofranco.1990@gmail.com>
2012-11-25 22:19:24 -0800

    fs: sync: enable fsync by default.
    Signed-off-by: franciscofranco <franciscofranco.1990@gmail.com>
    Signed-off-by: Francisco Franco <franciscofranco.1990@gmail.com>

--- fs/sync.c
+++ fs/sync.c
@@ -18,7 +18,7 @@
 #include <linux/backing-dev.h>
 #include "internal.h"
 
-bool fsync_enabled = false;
+bool fsync_enabled = true;
 module_param(fsync_enabled, bool, 0755);
 
 #define VALID_FLAGS (SYNC_FILE_RANGE_WAIT_BEFORE|SYNC_FILE_RANGE_WRITE| \
