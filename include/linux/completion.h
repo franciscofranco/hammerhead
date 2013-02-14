9f3e4e611748ade2c4d4379cd830fbc1cd0604e3
Vladimir Davydov <vdavydov@parallels.com>
2013-02-14 18:19:58 +0400

    sched: add wait_for_completion_io[_timeout]
    The only difference between wait_for_completion[_timeout]() and
    wait_for_completion_io[_timeout]() is that the latter calls
    io_schedule_timeout() instead of schedule_timeout() so that the caller
    is accounted as waiting for IO, not just sleeping.
    
    These functions can be used for correct iowait time accounting when the
    completion struct is actually used for waiting for IO (e.g. completion
    of a bio request in the block layer).
    
    Modified to compile with 3.4.0 on the Nexus 4 (Mako)
    
    Signed-off-by: Vladimir Davydov <vdavydov@parallels.com>
    Acked-by: Ingo Molnar <mingo@kernel.org>
    Signed-off-by: Jens Axboe <axboe@kernel.dk>
    Signed-off-by: Francisco Franco <franciscofranco.1990@gmail.com>

--- include/linux/completion.h
+++ include/linux/completion.h
@@ -82,6 +82,8 @@ extern int wait_for_completion_interruptible(struct completion *x);
 extern int wait_for_completion_killable(struct completion *x);
 extern unsigned long wait_for_completion_timeout(struct completion *x,
 						   unsigned long timeout);
+extern unsigned long wait_for_completion_io_timeout(struct completion *x,
+						    unsigned long timeout);
 extern long wait_for_completion_interruptible_timeout(
 	struct completion *x, unsigned long timeout);
 extern long wait_for_completion_killable_timeout(
