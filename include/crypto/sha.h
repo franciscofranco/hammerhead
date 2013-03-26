6c69c1cdf9ee59a3cb6dd48211708452837cbee4
Tim Chen <tim.c.chen@linux.intel.com>
2013-03-26 13:59:25 -0700

    crypto: sha512 - Expose generic sha512 routine to be callable from other modules
    Other SHA512 routines may need to use the generic routine when
    FPU is not available.
    
    Change-Id: I06ef572dc4fc968f9db8aa34be38c3eab26c8148
    Signed-off-by: Tim Chen <tim.c.chen@linux.intel.com>
    Signed-off-by: Herbert Xu <herbert@gondor.apana.org.au>
    Signed-off-by: franciscofranco <franciscofranco.1990@gmail.com>

--- include/crypto/sha.h
+++ include/crypto/sha.h
@@ -87,4 +87,6 @@ struct shash_desc;
 extern int crypto_sha1_update(struct shash_desc *desc, const u8 *data,
 			      unsigned int len);
 
+extern int crypto_sha512_update(struct shash_desc *desc, const u8 *data,
+			      unsigned int len);
 #endif
