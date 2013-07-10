cc5746f4fcc60bcba296d3664cc1086dd6b59172
Jack Pham <jackp@codeaurora.org>
2013-07-10 16:40:55 -0700

    usb: dwc3-msm: replace wakelock with wakeup source
    As the wakelock API is now banned, use the underlying
    wakeup_source-based implementation directly.
    
    Change-Id: Id6abe00f77b94b4fe82af19def5138e3286ed456
    Signed-off-by: Jack Pham <jackp@codeaurora.org>
    Signed-off-by: franciscofranco <franciscofranco.1990@gmail.com>

--- drivers/usb/dwc3/dwc3-msm.c
+++ drivers/usb/dwc3/dwc3-msm.c
@@ -35,6 +35,7 @@
 #include <linux/qpnp-misc.h>
 #include <linux/usb/msm_hsusb.h>
 #include <linux/regulator/consumer.h>
+#include <linux/pm_wakeup.h>
 #include <linux/power_supply.h>
 #include <linux/qpnp/qpnp-adc.h>
 #include <linux/slimport.h>
@@ -198,7 +199,6 @@ struct dwc3_msm {
 	int			hsphy_host_init_seq;
 	struct delayed_work	resume_work;
 	struct work_struct	restart_usb_work;
-	struct wake_lock	wlock;
 	struct dwc3_charger	charger;
 	struct usb_phy		*otg_xceiv;
 	struct delayed_work	chg_work;
@@ -1791,7 +1791,7 @@ static int dwc3_msm_suspend(struct dwc3_msm *mdwc)
 		enable_irq_wake(mdwc->hs_phy_irq);
 	}
 
-	wake_unlock(&mdwc->wlock);
+	pm_relax(mdwc->dev);
 	atomic_set(&mdwc->in_lpm, 1);
 
 	dev_info(mdwc->dev, "DWC3 in low power mode\n");
@@ -1812,7 +1812,7 @@ static int dwc3_msm_resume(struct dwc3_msm *mdwc)
 		return 0;
 	}
 
-	wake_lock(&mdwc->wlock);
+	pm_stay_awake(mdwc->dev);
 
 	if (mdwc->bus_perf_client) {
 		ret = msm_bus_scale_client_update_request(
@@ -2774,8 +2774,8 @@ static int __devinit dwc3_msm_probe(struct platform_device *pdev)
 		msm->otg_xceiv = NULL;
 	}
 
-	wake_lock_init(&msm->wlock, WAKE_LOCK_SUSPEND, "msm_dwc3");
-	wake_lock(&msm->wlock);
+	device_init_wakeup(msm->dev, 1);
+	pm_stay_awake(msm->dev);
 	dwc3_debugfs_init(msm);
 
 	return 0;
@@ -2841,7 +2841,7 @@ static int __devexit dwc3_msm_remove(struct platform_device *pdev)
 		regulator_disable(msm->vbus_otg);
 
 	pm_runtime_disable(msm->dev);
-	wake_lock_destroy(&msm->wlock);
+	device_init_wakeup(msm->dev, 0);
 
 	dwc3_hsusb_ldo_enable(0);
 	dwc3_hsusb_ldo_init(0);
