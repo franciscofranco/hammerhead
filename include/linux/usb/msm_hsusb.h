41c804705c5d0bd9b5418e974ff1e8408b64d49c
Mayank Rana <mrana@codeaurora.org>
2013-07-24 17:02:45 +0530

    USB: msm_otg: Add voting for sleep clock
    USB PHY is provided sleep clock on few platform which is
    required to enable for USB PHY functionality. Hence add
    support to vote sleep clock if it exists.
    
    CRs-Fixed: 518353
    Change-Id: I67006bfe3fbe9b3ad3e1d1e2ddb0e8e060437bd7
    Signed-off-by: Mayank Rana <mrana@codeaurora.org>
    Signed-off-by: franciscofranco <franciscofranco.1990@gmail.com>

--- include/linux/usb/msm_hsusb.h
+++ include/linux/usb/msm_hsusb.h
@@ -307,6 +307,7 @@ struct msm_otg_platform_data {
  * @pclk: clock struct of iface_clk.
  * @phy_reset_clk: clock struct of phy_clk.
  * @core_clk: clock struct of core_bus_clk.
+ * @sleep_clk: clock struct of sleep_clk for USB PHY.
  * @core_clk_rate: core clk max frequency
  * @regs: ioremapped register base address.
  * @inputs: OTG state machine inputs(Id, SessValid etc).
@@ -343,6 +344,7 @@ struct msm_otg {
 	struct clk *pclk;
 	struct clk *phy_reset_clk;
 	struct clk *core_clk;
+	struct clk *sleep_clk;
 	long core_clk_rate;
 	void __iomem *regs;
 #define ID		0
