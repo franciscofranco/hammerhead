900d050ff2b2b4eccec8b9aaaba429bf2a4c9b62
Francisco Franco <franciscofranco.1990@gmail.com>
2013-09-25 03:03:01 +0100

    audit: kiss goodbye you stupid piece of crap logging messages.
    Signed-off-by: Francisco Franco <franciscofranco.1990@gmail.com>

--- kernel/audit.c
+++ kernel/audit.c
@@ -69,7 +69,7 @@
 #define AUDIT_DISABLED		-1
 #define AUDIT_UNINITIALIZED	0
 #define AUDIT_INITIALIZED	1
-static int	audit_initialized;
+static int	audit_initialized = AUDIT_DISABLED;
 
 #define AUDIT_OFF	0
 #define AUDIT_ON	1
