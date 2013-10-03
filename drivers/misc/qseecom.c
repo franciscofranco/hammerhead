3043075751634418818f5f71af4e74fb8cae36e4
Zhen Kong <zkong@codeaurora.org>
2013-10-03 13:58:45 -0700

    qseecom: Remove debug  messages
    Remove unnecessary debug  messages.
    
    Change-Id: Ida80fbc1376f15753f8329c44550b14d8f4de163
    Signed-off-by: Mona Hossain <mhossain@codeaurora.org>
    Signed-off-by: Zhen Kong <zkong@codeaurora.org>
    Signed-off-by: franciscofranco <franciscofranco.1990@gmail.com>

--- drivers/misc/qseecom.c
+++ drivers/misc/qseecom.c
@@ -1060,8 +1060,6 @@ static int qseecom_send_cmd(struct qseecom_dev_handle *data, void __user *argp)
 	if (ret)
 		return ret;
 
-	pr_debug("sending cmd_req->rsp size: %u, ptr: 0x%p\n",
-			req.resp_len, req.resp_buf);
 	return ret;
 }
 
@@ -1251,8 +1249,7 @@ static int qseecom_send_modfd_cmd(struct qseecom_dev_handle *data,
 	ret = __qseecom_update_cmd_buf(&req, true, data, false);
 	if (ret)
 		return ret;
-	pr_debug("sending cmd_req->rsp size: %u, ptr: 0x%p\n",
-			req.resp_len, req.resp_buf);
+
 	return ret;
 }
 
