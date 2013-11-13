33dac936dd839354359e2f0002136749d1e0339e
Francisco Franco <franciscofranco.1990@gmail.com>
2013-11-13 01:21:32 +0000

    Revert "suspend: remove disable/enable cpus every time suspend is called (which is called every damn 5 seconds -.-)."
    This reverts commit 450a7acdebdc691937c8a2ed9515bab598358b21.

--- kernel/power/suspend.c
+++ kernel/power/suspend.c
@@ -198,6 +198,10 @@ static int suspend_enter(suspend_state_t state, bool *wakeup)
 	if (suspend_test(TEST_PLATFORM))
 		goto Platform_wake;
 
+	error = disable_nonboot_cpus();
+	if (error || suspend_test(TEST_CPUS))
+		goto Enable_cpus;
+
 	arch_suspend_disable_irqs();
 	BUG_ON(!irqs_disabled());
 
@@ -213,6 +217,9 @@ static int suspend_enter(suspend_state_t state, bool *wakeup)
 
 	arch_suspend_enable_irqs();
 	BUG_ON(irqs_disabled());
+
+ Enable_cpus:
+	enable_nonboot_cpus();
 
  Platform_wake:
 	if (need_suspend_ops(state) && suspend_ops->wake)
