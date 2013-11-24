96443adb96eef776d1e970230f04b6c53aca5654
myfluxi <linflux@arcor.de>
2013-11-24 22:24:07 +0100

    msm: mdss: Remove tunable for command 32
    Even slight changes look ugly or distort the screen.
    
    Signed-off-by: Francisco Franco <franciscofranco.1990@gmail.com>

--- drivers/video/msm/mdss/mdss_dsi_panel.c
+++ drivers/video/msm/mdss/mdss_dsi_panel.c
@@ -1160,7 +1160,6 @@ read_one(kgamma_20, 20);
 read_one(kgamma_22, 22);
 read_one(kgamma_28, 28);
 read_one(kgamma_31, 31);
-read_one(kgamma_32, 32);
 
 #define write_one(file_name, cmd)				\
 static ssize_t write_##file_name				\
@@ -1180,7 +1179,6 @@ write_one(kgamma_20, 20);
 write_one(kgamma_22, 22);
 write_one(kgamma_28, 28);
 write_one(kgamma_31, 31);
-write_one(kgamma_32, 32);
 
 #define define_one_rw(_name)					\
 static DEVICE_ATTR(_name, 0644, read_##_name, write_##_name);
@@ -1195,7 +1193,6 @@ define_one_rw(kgamma_20);
 define_one_rw(kgamma_22);
 define_one_rw(kgamma_28);
 define_one_rw(kgamma_31);
-define_one_rw(kgamma_32);
 
 static struct attribute *dsi_panel_attributes[] = {
 	&dev_attr_kgamma_r.attr,
@@ -1208,7 +1205,6 @@ static struct attribute *dsi_panel_attributes[] = {
 	&dev_attr_kgamma_22.attr,
 	&dev_attr_kgamma_28.attr,
 	&dev_attr_kgamma_31.attr,
-	&dev_attr_kgamma_32.attr,
 	NULL
 };
 
