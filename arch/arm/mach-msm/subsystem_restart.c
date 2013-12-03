0435532b13c2a418dc86f05345ee42ac6fb103e9
Deepak Katragadda <dkatraga@codeaurora.org>
2013-12-02 16:28:59 -0800

    msm: subsystem_restart: Add notification to indicate powerup failure
    Currently, if powering on a subsystem during restart fails, a
    kernel panic is trigerred immediately.
    Instead, send a 'powerup failure' notification to the drivers
    that have registered for notifications regarding the subsystem.
    The drivers can then do the necessary cleanup/logging before a
    kernel panic is triggered.
    
    Change-Id: I252f221ee009436b7075c203b2a3de45e395c1b0
    Signed-off-by: Deepak Katragadda <dkatraga@codeaurora.org>
    Signed-off-by: franciscofranco <franciscofranco.1990@gmail.com>

--- arch/arm/mach-msm/subsystem_restart.c
+++ arch/arm/mach-msm/subsystem_restart.c
@@ -1,4 +1,4 @@
-/* Copyright (c) 2011-2013, The Linux Foundation. All rights reserved.
+/* Copyright (c) 2011-2014, The Linux Foundation. All rights reserved.
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License version 2 and
@@ -479,6 +479,8 @@ static void subsystem_powerup(struct subsys_device *dev, void *data)
 #ifdef CONFIG_LGE_HANDLE_PANIC
 		lge_set_magic_subsystem(name, LGE_ERR_SUB_PWR);
 #endif
+		notify_each_subsys_device(&dev, 1, SUBSYS_POWERUP_FAILURE,
+								NULL);
 		panic("[%p]: Powerup error: %s!", current, name);
 	}
 
@@ -487,10 +489,11 @@ static void subsystem_powerup(struct subsys_device *dev, void *data)
 #ifdef CONFIG_LGE_HANDLE_PANIC
 		lge_set_magic_subsystem(name, LGE_ERR_SUB_PWR);
 #endif
+		notify_each_subsys_device(&dev, 1, SUBSYS_POWERUP_FAILURE,
+								NULL);
 		panic("[%p]: Timed out waiting for error ready: %s!",
 			current, name);
 	}
-
 	subsys_set_state(dev, SUBSYS_ONLINE);
 }
 
