bbd1533c7d2f8c7cebf1dff7e107ab2d4b2b280a
Vikash Garodia <vgarodia@codeaurora.org>
2014-01-07 18:20:03 +0530

    msm: vidc: add sleep of 100us before clock disable
    ARM9 execute following sequence when venus is idle
    1. Disable ARM9 watchdog
    2. Set bit 30 of A_VENUS_CPU_CS_SCIACMDARG0 register
       to indicate venus is idle
    3. Write HFI_MSG_SYS_IDLE to message queue
    4. Raise interrupt to host
    5. ARM9 execute wait for interrupt instruction to go
       into low power state. This will internally drain the
       ARM9 write buffer which can generate bus transaction.
    6. ARM9 in idle
    
    Host clock gate venus based on following sequence
    1. Host receive HFI_MSG_SYS_IDLE from the msg queue
    2. Host check both cmd / msg queue are empty by
       comparing the queue read/write pointers.
    3. Host check bit 30 of A_VENUS_CPU_CS_SCIACMDARG0
       register to confirm it indicate idle
    4. Host clock gate whole venus
    
    As the host clock gate venus in step 4, ARM9 is still
    in step 5&6. The sleep will ensure that step 5 and 6
    are completed before host clock gates venus.
    
    CRs-Fixed: 589417
    CRs-Fixed: 595251
    Change-Id: Idf1f788718adea2dacceca97afa4ba4fe4562ddd
    Signed-off-by: Vikash Garodia <vgarodia@codeaurora.org>
    Signed-off-by: franciscofranco <franciscofranco.1990@gmail.com>

--- drivers/media/platform/msm/vidc/venus_hfi.c
+++ drivers/media/platform/msm/vidc/venus_hfi.c
@@ -837,6 +837,7 @@ static inline int venus_hfi_clk_enable(struct venus_hfi_device *device)
 fail_clk_enable:
 	for (i--; i >= 0; i--) {
 		cl = &device->resources.clock[i];
+		usleep(100);
 		clk_disable(cl->clk);
 	}
 	return rc;
@@ -859,6 +860,7 @@ static inline void venus_hfi_clk_disable(struct venus_hfi_device *device)
 
 	for (i = 0; i <= device->clk_gating_level; i++) {
 		cl = &device->resources.clock[i];
+		usleep(100);
 		clk_disable(cl->clk);
 	}
 	device->clocks_enabled = 0;
@@ -2755,12 +2757,14 @@ static inline void venus_hfi_disable_clks(struct venus_hfi_device *device)
 	if (device->clocks_enabled) {
 		for (i = VCODEC_CLK; i < VCODEC_MAX_CLKS; i++) {
 			cl = &device->resources.clock[i];
+			usleep(100);
 			clk_disable(cl->clk);
 		}
 	} else {
 		for (i = device->clk_gating_level + 1;
 			i < VCODEC_MAX_CLKS; i++) {
 			cl = &device->resources.clock[i];
+			usleep(100);
 			clk_disable(cl->clk);
 		}
 	}
@@ -2803,6 +2807,7 @@ static inline int venus_hfi_enable_clks(struct venus_hfi_device *device)
 fail_clk_enable:
 	for (; i >= 0; i--) {
 		cl = &device->resources.clock[i];
+		usleep(100);
 		clk_disable_unprepare(cl->clk);
 	}
 	mutex_unlock(&device->clk_pwr_lock);
