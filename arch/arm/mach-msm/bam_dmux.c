e79c58b6d3c9b6d49301bc7e5fb71b81678287eb
franciscofranco <franciscofranco.1990@gmail.com>
2014-02-10 04:31:47 +0000

    msm: bam_dmux: increase min/max sleep usecs. Remove spinlocks around the wake_lock/unlock and use an atomic variable instead to protect wakelock_reference_count.
    Signed-off-by: franciscofranco <franciscofranco.1990@gmail.com>

--- arch/arm/mach-msm/bam_dmux.c
+++ arch/arm/mach-msm/bam_dmux.c
@@ -52,10 +52,10 @@
 static int msm_bam_dmux_debug_enable;
 module_param_named(debug_enable, msm_bam_dmux_debug_enable,
 		   int, S_IRUGO | S_IWUSR | S_IWGRP);
-static int POLLING_MIN_SLEEP = 2950;
+static int POLLING_MIN_SLEEP = 4950;
 module_param_named(min_sleep, POLLING_MIN_SLEEP,
 		   int, S_IRUGO | S_IWUSR | S_IWGRP);
-static int POLLING_MAX_SLEEP = 3050;
+static int POLLING_MAX_SLEEP = 5000;
 module_param_named(max_sleep, POLLING_MAX_SLEEP,
 		   int, S_IRUGO | S_IWUSR | S_IWGRP);
 static int POLLING_INACTIVITY = 1;
@@ -260,8 +260,7 @@ static DEFINE_MUTEX(dfab_status_lock);
 static int dfab_is_on;
 static int wait_for_dfab;
 static struct completion dfab_unvote_completion;
-static DEFINE_SPINLOCK(wakelock_reference_lock);
-static int wakelock_reference_count;
+static atomic_t wakelock_reference_count = ATOMIC_INIT(0);
 static int a2_pc_disabled_wakelock_skipped;
 static int disconnect_ack = 1;
 static LIST_HEAD(bam_other_notify_funcs);
@@ -1935,34 +1934,36 @@ static void unvote_dfab(void)
 /* reference counting wrapper around wakelock */
 static void grab_wakelock(void)
 {
-	unsigned long flags;
+	int wakelock_count = atomic_read(&wakelock_reference_count);
 
-	spin_lock_irqsave(&wakelock_reference_lock, flags);
 	BAM_DMUX_LOG("%s: ref count = %d\n", __func__,
-						wakelock_reference_count);
-	if (wakelock_reference_count == 0)
+						wakelock_count);
+
+	if (!wakelock_count)
 		wake_lock(&bam_wakelock);
-	++wakelock_reference_count;
-	spin_unlock_irqrestore(&wakelock_reference_lock, flags);
+
+	atomic_inc(&wakelock_reference_count);
 }
 
 static void release_wakelock(void)
 {
-	unsigned long flags;
+	int wakelock_count = atomic_read(&wakelock_reference_count);
 
-	spin_lock_irqsave(&wakelock_reference_lock, flags);
-	if (wakelock_reference_count == 0) {
+	if (!wakelock_count) 
+	{
 		DMUX_LOG_KERR("%s: bam_dmux wakelock not locked\n", __func__);
-		dump_stack();
-		spin_unlock_irqrestore(&wakelock_reference_lock, flags);
 		return;
 	}
+
 	BAM_DMUX_LOG("%s: ref count = %d\n", __func__,
-						wakelock_reference_count);
-	--wakelock_reference_count;
-	if (wakelock_reference_count == 0)
+						wakelock_count);
+
+	atomic_dec(&wakelock_reference_count);
+
+	wakelock_count = atomic_read(&wakelock_reference_count);
+
+	if (!wakelock_count)
 		wake_unlock(&bam_wakelock);
-	spin_unlock_irqrestore(&wakelock_reference_lock, flags);
 }
 
 static int restart_notifier_cb(struct notifier_block *this,
