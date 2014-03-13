6efb863579846ade63dbc28428ae6adc5d600b41
franciscofranco <franciscofranco.1990@gmail.com>
2014-03-13 15:24:22 +0000

    msm: use interruptible threads to decrease overall load_avg.
    Signed-off-by: franciscofranco <franciscofranco.1990@gmail.com>

--- drivers/video/msm/mdss/mdss_fb.c
+++ drivers/video/msm/mdss/mdss_fb.c
@@ -1519,7 +1519,7 @@ static int __mdss_fb_display_thread(void *data)
 		pr_info("%s: set priority failed\n", __func__);
 
 	while (1) {
-		wait_event(mfd->commit_wait_q,
+		wait_event_interruptible(mfd->commit_wait_q,
 				atomic_read(&mfd->commits_pending));
 
 		ret = __mdss_fb_perform_commit(mfd);
