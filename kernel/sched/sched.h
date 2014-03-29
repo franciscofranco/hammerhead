e124d306101eb964d3553971c87abaeaf94162fc
Srivatsa Vaddagiri <vatsa@codeaurora.org>
2014-03-29 11:40:16 -0700

    sched: window-based load stats improvements
    Following cleanups and improvements are made to window-based load
    stats feature:
    
    * Add sysctl to pick max, avg or most recent samples as task's
      demand.
    
    * Fix overflow possibility in calculation of sum for average policy.
    
    * Use unscaled statistics when a task is running on a CPU which is
    thermally throttled.
    
    Change-Id: I8293565ca0c2a785dadf8adb6c67f579a445ed29
    Signed-off-by: Srivatsa Vaddagiri <vatsa@codeaurora.org>
    Signed-off-by: franciscofranco <franciscofranco.1990@gmail.com>

--- kernel/sched/sched.h
+++ kernel/sched/sched.h
@@ -8,9 +8,6 @@
 
 extern __read_mostly int scheduler_running;
 
-extern unsigned int max_possible_freq;
-extern unsigned int min_max_freq;
-
 /*
  * Convert user-nice values [ -20 ... 0 ... 19 ]
  * to static priority [ MAX_RT_PRIO..MAX_PRIO-1 ],
@@ -559,6 +556,8 @@ DECLARE_PER_CPU(int, sd_llc_id);
 #include "auto_group.h"
 
 extern unsigned int sched_ravg_window;
+extern unsigned int max_possible_freq;
+extern unsigned int min_max_freq;
 extern unsigned int pct_task_load(struct task_struct *p);
 extern void init_new_task_load(struct task_struct *p);
 
