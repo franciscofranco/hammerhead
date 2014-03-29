d1358bd46cd123496cff2042629275441d1e86be
Srivatsa Vaddagiri <vatsa@codeaurora.org>
2014-03-29 11:40:16 -0700

    sched: Window-based load stat improvements
    Some tasks can have a sporadic load pattern such that they can suddenly
    start running for longer intervals of time after running for shorter
    durations. To recognize such sharp increase in tasks' demands, max
    between the average of 5 window load samples and the most recent sample
    is chosen as the task demand.
    
    Make the window size (sched_ravg_window) configurable at boot up
    time. To prevent users from setting inappropriate values for window
    size, min and max limits are defined. As 'ravg' struct tracks load for
    both real-time and non real-time tasks it is moved out of sched_entity
    struct.
    
    In order to prevent changing function signatures for move_tasks() and
    move_one_task() per-cpu variables are defined to track the total load
    moved. In case multiple tasks are selected to migrate in one load
    balance operation, loads > 100 could be sent through migration notifiers.
    Prevent this scenario by setting mnd.load to 100 in such cases.
    
    Define wrapper functions to compute cpu demands for tasks and to change
    rq->cumulative_runnable_avg.
    
    Change-Id: I9abfbf3b5fe23ae615a6acd3db9580cfdeb515b4
    Signed-off-by: Srivatsa Vaddagiri <vatsa@codeaurora.org>
    Signed-off-by: Rohit Gupta <rohgup@codeaurora.org>
    Signed-off-by: franciscofranco <franciscofranco.1990@gmail.com>

--- include/linux/sched.h
+++ include/linux/sched.h
@@ -1247,11 +1247,6 @@ struct ravg {
 
 struct sched_entity {
 	struct load_weight	load;		/* for load-balancing */
-	/*
-	 * Todo : Move ravg to 'struct task_struct', as this is common for both
-	 * real-time and non-realtime tasks
-	 */
-	struct ravg		ravg;
 	struct rb_node		run_node;
 	struct list_head	group_node;
 	unsigned int		on_rq;
@@ -1334,6 +1329,7 @@ struct task_struct {
 	const struct sched_class *sched_class;
 	struct sched_entity se;
 	struct sched_rt_entity rt;
+	struct ravg ravg;
 
 #ifdef CONFIG_PREEMPT_NOTIFIERS
 	/* list of struct preempt_notifier: */
@@ -2112,7 +2108,6 @@ extern unsigned int sysctl_sched_min_granularity;
 extern unsigned int sysctl_sched_wakeup_granularity;
 extern unsigned int sysctl_sched_child_runs_first;
 extern unsigned int sysctl_sched_wake_to_idle;
-extern unsigned int sysctl_sched_ravg_window;
 extern unsigned int sysctl_sched_wakeup_load_threshold;
 
 enum sched_tunable_scaling {
