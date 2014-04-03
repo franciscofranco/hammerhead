009497ff229952bf4f4069f9e7c25692c38d3bf5
franciscofranco <franciscofranco.1990@gmail.com>
2014-04-03 01:18:16 +0100

    Revert "sched/fair: improve fairness efficiency"
    This reverts commit be7f7ce5ddb192e156ff569268a1289435499e7d.

--- kernel/sched/fair.c
+++ kernel/sched/fair.c
@@ -596,8 +596,9 @@ int sched_proc_update_handler(struct ctl_table *table, int write,
 static inline unsigned long
 calc_delta_fair(unsigned long delta, struct sched_entity *se)
 {
-	/* nice? why bother, MAX delta here! */
-	delta = LONG_MAX;
+	if (unlikely(se->load.weight != NICE_0_LOAD))
+		delta = calc_delta_mine(delta, NICE_0_LOAD, &se->load);
+
 	return delta;
 }
 
@@ -615,8 +616,7 @@ static u64 __sched_period(unsigned long nr_running)
 	unsigned long nr_latency = sched_nr_latency;
 
 	if (unlikely(nr_running > nr_latency)) {
-		/* let's give real granularity here */
-		period = 5000000ULL;
+		period = sysctl_sched_min_granularity;
 		period *= nr_running;
 	}
 
