8752aed59b85396884d4168acadfab99e0bcffd6
franciscofranco <franciscofranco.1990@gmail.com>
2014-05-01 22:48:12 +0100

    cpufreq: interactive: export cpu load.
    Signed-off-by: franciscofranco <franciscofranco.1990@gmail.com>

--- drivers/cpufreq/cpufreq_interactive.c
+++ drivers/cpufreq/cpufreq_interactive.c
@@ -397,6 +397,8 @@ static void cpufreq_interactive_timer(unsigned long data)
 	pcpu->prev_load = cpu_load;
 	boosted = now < (last_input_time + boostpulse_duration_val);
 
+	cpufreq_notify_utilization(pcpu->policy, cpu_load);
+
 	if (cpu_load >= go_hispeed_load)
 	{
 		if (pcpu->target_freq < hispeed_freq) 
