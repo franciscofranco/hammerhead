beeae5a51cf67f6f164d54a3c360944d8e313c5e
Asutosh Das <asutoshd@codeaurora.org>
2014-05-05 14:31:11 +0530

    mmc: core: abort rescan if suspend is triggered
    There could be a race between suspend and mmc_rescan as follows:
    At resume:
    -> mmc_pm_notify
      -> mmc_rescan is scheduled
    
    Suspend is triggered:
    -> mmc_pm_notify
       -> wait for mmc_rescan/cancel it
          -> rescan acquires wakelock if card is detected
             so as to give user-space a chance to respond
    -> PM framework cancels suspend since wakelock is active
    
    Hence, release the wake-lock if its active in suspend prepare
    and check for rescan_disable before acquiring wake-lock.
    This prefers suspend over notifying the user-space of the status
    of the card, which is fine since with suspend user-space is frozen
    too.
    
    CRs-fixed: 655281
    Change-Id: I83783589f9b541d3af73931e9a7b0b144a25538e
    Signed-off-by: Asutosh Das <asutoshd@codeaurora.org>
    Signed-off-by: franciscofranco <franciscofranco.1990@gmail.com>

--- drivers/mmc/core/sd.c
+++ drivers/mmc/core/sd.c
@@ -1385,7 +1385,11 @@ int mmc_attach_sd(struct mmc_host *host)
 	 */
 #ifdef CONFIG_MMC_PARANOID_SD_INIT
 	retries = 5;
-	while (retries) {
+	/*
+	 * Some bad cards may take a long time to init, give preference to
+	 * suspend in those cases.
+	 */
+	while (retries && !host->rescan_disable) {
 		err = mmc_sd_init_card(host, host->ocr, NULL);
 		if (err) {
 			retries--;
@@ -1403,6 +1407,9 @@ int mmc_attach_sd(struct mmc_host *host)
 		       mmc_hostname(host), err);
 		goto err;
 	}
+
+	if (host->rescan_disable)
+		goto err;
 #else
 	err = mmc_sd_init_card(host, host->ocr, NULL);
 	if (err)
@@ -1426,9 +1433,9 @@ remove_card:
 	mmc_claim_host(host);
 err:
 	mmc_detach_bus(host);
-
-	pr_err("%s: error %d whilst initialising SD card\n",
-		mmc_hostname(host), err);
+	if (err)
+		pr_err("%s: error %d whilst initialising SD card: rescan: %d\n",
+		       mmc_hostname(host), err, host->rescan_disable);
 
 	return err;
 }
