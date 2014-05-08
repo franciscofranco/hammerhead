a859976279589f86edd5ddc64e5b01d7dbf33f4b
myfluxi <linflux@arcor.de>
2014-05-08 18:12:00 +0200

    cpufreq: interactive: Use parts of native boost routine
    Not all governors are created equal. Set boostpulse_endtime
    and floor_freq to smoothen the timer routine.
    
    [franciscofranco] removed boostpulse_endtime from the
    original commit because I'm not using it.
    
    Signed-off-by: franciscofranco <franciscofranco.1990@gmail.com>

--- drivers/cpufreq/cpufreq_interactive.c
+++ drivers/cpufreq/cpufreq_interactive.c
@@ -1300,8 +1300,11 @@ static int cpufreq_governor_interactive(struct cpufreq_policy *policy,
 			spin_lock_irqsave(&pcpu->target_freq_lock, flags);
 			if (policy->max < pcpu->target_freq)
 				pcpu->target_freq = policy->max;
-			else if (policy->min > pcpu->target_freq)
+			else if (policy->min > pcpu->target_freq) {
 				pcpu->target_freq = policy->min;
+				pcpu->floor_freq = policy->min;
+				pcpu->floor_validate_time = ktime_to_us(ktime_get());
+			}
 
 			spin_unlock_irqrestore(&pcpu->target_freq_lock, flags);
 			up_read(&pcpu->enable_sem);
