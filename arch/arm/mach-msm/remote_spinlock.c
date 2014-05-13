91a23f8e903d7f732b4e0a6cf7afbabcd89871d7
myfluxi <linflux@arcor.de>
2014-05-13 23:09:19 +0200

    msm: remote_spinlock: swp{b} is deprecated for ARM>v5
    Change-Id: I7456207cb9ee38d58e4f9743a3a2a7637363d4fe
    Signed-off-by: Francisco Franco <franciscofranco.1990@gmail.com>

--- arch/arm/mach-msm/remote_spinlock.c
+++ arch/arm/mach-msm/remote_spinlock.c
@@ -144,7 +144,7 @@ static int __raw_remote_dek_spin_owner(raw_remote_spinlock_t *lock)
 }
 /* end dekkers implementation ----------------------------------------------- */
 
-#ifndef CONFIG_THUMB2_KERNEL
+#if (__LINUX_ARM_ARCH__ <= 5)
 /* swp implementation ------------------------------------------------------- */
 static void __raw_remote_swp_spin_lock(raw_remote_spinlock_t *lock)
 {
@@ -434,7 +434,7 @@ static void initialize_ops(void)
 		current_ops.owner = __raw_remote_dek_spin_owner;
 		is_hw_lock_type = 0;
 		break;
-#ifndef CONFIG_THUMB2_KERNEL
+#if (__LINUX_ARM_ARCH__ <= 5)
 	case SWP_MODE:
 		current_ops.lock = __raw_remote_swp_spin_lock;
 		current_ops.unlock = __raw_remote_swp_spin_unlock;
