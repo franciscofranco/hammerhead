8f0d12c929684f469d1f3c363e79e22c727f49ee
ChandanaKishori Chiluveru <cchilu@codeaurora.org>
2014-06-03 15:31:18 +0530

    usb: android: Skip the work when set_config arrives with same value twice
    Some USB hosts may send SET_CONFIGURATION with same value twice.
    The current code implementation processing the 2nd set_config and
    schedules the work. The android_work() issues disconnect event to
    userspace if SetConfiguration with non-zero value is sent twice.
    The userspace applications may disconnect the USB and the device
    doesn't connect to the host.
    
    Fix this by avoiding the android_work for the second time SET_CONFIGURATION
    request when it receives SetConfiguration request with same configuration
    value twice from the host.
    
    CRs-Fixed: 661220
    Change-Id: Ic450315a9067b9b679b4936480d125e4e4b7cdf7
    Signed-off-by: ChandanaKishori Chiluveru <cchilu@codeaurora.org>
    Signed-off-by: D. Andrei Măceș <Andrei.Maces@alumni.nd.edu>
    Signed-off-by: Francisco Franco <franciscofranco.1990@gmail.com>

--- drivers/usb/gadget/android.c
+++ drivers/usb/gadget/android.c
@@ -3205,6 +3205,7 @@ android_setup(struct usb_gadget *gadget, const struct usb_ctrlrequest *c)
 	int value = -EOPNOTSUPP;
 	unsigned long flags;
 	bool do_work = false;
+	bool prev_configured = false;
 
 	req->zero = 0;
 	req->complete = composite_setup_complete;
@@ -3223,6 +3224,12 @@ android_setup(struct usb_gadget *gadget, const struct usb_ctrlrequest *c)
 			}
 		}
 
+	/*
+	 * skip the  work when 2nd set config arrives
+	 * with same value from the host.
+	 */
+	if (cdev->config)
+		prev_configured = true;
 	/* Special case the accessory function.
 	 * It needs to handle control requests before it is enabled.
 	 */
@@ -3238,7 +3245,8 @@ android_setup(struct usb_gadget *gadget, const struct usb_ctrlrequest *c)
 		do_work = true;
 	} else if (c->bRequest == USB_REQ_SET_CONFIGURATION &&
 						cdev->config) {
-		do_work = true;
+		if (!prev_configured)
+			do_work = true;
 	}
 	spin_unlock_irqrestore(&cdev->lock, flags);
 	if (do_work)
