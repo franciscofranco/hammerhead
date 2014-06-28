ce8d086b628ac1f8917cf7340b57fed992e31cfa
franciscofranco <franciscofranco.1990@gmail.com>
2014-06-28 23:28:26 +0000

    cpufreq: interactive: fix another derp, I was probably drunk when I merged the patch before.
    Signed-off-by: franciscofranco <franciscofranco.1990@gmail.com>

--- drivers/cpufreq/cpufreq_interactive.c
+++ drivers/cpufreq/cpufreq_interactive.c
@@ -458,14 +458,16 @@ static void cpufreq_interactive_timer(unsigned long data)
 		{
 			new_freq = choose_freq(pcpu, loadadjfreq);
 
-			if (new_freq > boosted_freq &&
-					pcpu->target_freq < hispeed_freq)
+			if (new_freq < boosted_freq)
 				new_freq = boosted_freq;
 		}
 	}
 	else
 	{
 		new_freq = choose_freq(pcpu, loadadjfreq);
+		if (new_freq > boosted_freq &&
+					pcpu->target_freq < boosted_freq)
+			new_freq = boosted_freq;
 
 		if (sync_freq && new_freq < sync_freq) {
 
