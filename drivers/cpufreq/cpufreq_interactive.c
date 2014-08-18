bc9711af6816f4094a06e6bbf6bc3082e9f3cd1a
franciscofranco <franciscofranco.1990@gmail.com>
2014-08-18 19:32:27 +0100

    cpufreq: interactive: let's try and live without the idle notifier and rely on the timer instead. Based on @crpalmer 's patch
    Signed-off-by: franciscofranco <franciscofranco.1990@gmail.com>

--- drivers/cpufreq/cpufreq_interactive.c
+++ drivers/cpufreq/cpufreq_interactive.c
@@ -160,6 +160,7 @@ static unsigned int sync_freq = CPU_SYNC_FREQ;
 static unsigned int up_threshold_any_cpu_freq = 1190400;
 
 #define DOWN_LOW_LOAD_THRESHOLD 5
+static bool idle_notifier = false;
 
 static void cpufreq_interactive_timer_resched(
 	struct cpufreq_interactive_cpuinfo *pcpu)
@@ -592,7 +593,7 @@ rearm_if_notmax:
 	 * Already set max speed and don't see a need to change that,
 	 * wait until next idle to re-evaluate, don't need timer.
 	 */
-	if (pcpu->target_freq == pcpu->policy->max)
+	if (idle_notifier && pcpu->target_freq == pcpu->policy->max)
 		goto exit;
 
 rearm:
@@ -1379,8 +1380,13 @@ static int cpufreq_governor_interactive(struct cpufreq_policy *policy,
 					&thread_migration_nb);
 
 		idle_notifier_register(&cpufreq_interactive_idle_nb);
-		cpufreq_register_notifier(
-			&cpufreq_notifier_block, CPUFREQ_TRANSITION_NOTIFIER);
+
+		if (idle_notifier)
+		{
+			cpufreq_register_notifier(
+				&cpufreq_notifier_block, CPUFREQ_TRANSITION_NOTIFIER);
+		}
+
 		mutex_unlock(&gov_lock);
 		break;
 
@@ -1405,8 +1411,12 @@ static int cpufreq_governor_interactive(struct cpufreq_policy *policy,
 				&migration_notifier_head,
 				&thread_migration_nb);
 
-		cpufreq_unregister_notifier(
-			&cpufreq_notifier_block, CPUFREQ_TRANSITION_NOTIFIER);
+		if (idle_notifier)
+		{
+			cpufreq_unregister_notifier(
+				&cpufreq_notifier_block, CPUFREQ_TRANSITION_NOTIFIER);
+		}
+
 		idle_notifier_unregister(&cpufreq_interactive_idle_nb);
 		sysfs_remove_group(cpufreq_global_kobject,
 				&interactive_attr_group);
