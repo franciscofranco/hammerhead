faeabc63690add1cb38c878a3f519c3db840b1d9
franciscofranco <franciscofranco.1990@gmail.com>
2014-09-08 00:03:57 +0100

    mako_hotplug: bugfixing
    * fix a bug where cpu_smash()->cpus_cpufreq_work()->cpus_offline_work()
    was being called every 100ms
    
    Signed-off-by: franciscofranco <franciscofranco.1990@gmail.com>

--- arch/arm/kernel/mako_hotplug.c
+++ arch/arm/kernel/mako_hotplug.c
@@ -135,10 +135,8 @@ inline static bool cpus_cpufreq_work(void)
 
 	for (cpu = 2; cpu < 4; cpu++)
 	{
-		current_freq |= cpufreq_quick_get(cpu);
+		current_freq += cpufreq_quick_get(cpu);
 	}
-
-	pr_info("%s: %d\n", __func__, current_freq);
 
 	return (current_freq >> 1) >= t->cpufreq_unplug_limit;
 }
@@ -215,7 +213,7 @@ static void cpu_smash(void)
 static void __ref decide_hotplug_func(struct work_struct *work)
 {
 	struct hotplug_tunables *t = &tunables;
-	unsigned int cur_load = 0;
+	unsigned long cur_load = 0;
 	unsigned int cpu;
 
 	/*
@@ -253,14 +251,16 @@ static void __ref decide_hotplug_func(struct work_struct *work)
 		if (stats.counter < t->max_load_counter)
 			++stats.counter;
 
-		cpu_revive(cur_load);
+		if (stats.online_cpus <= 2)
+			cpu_revive(cur_load);
 	}
 	else
 	{
 		if (stats.counter)
 			--stats.counter;
 
-		cpu_smash();
+		if (stats.online_cpus > 2)
+			cpu_smash();
 	}
 
 reschedule:
