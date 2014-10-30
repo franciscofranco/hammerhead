cf473a8dfde686f3a93e461977f010b79d9443aa
franciscofranco <franciscofranco.1990@gmail.com>
2014-10-30 22:32:18 +0000

    mako_hotplug: fix a rare case where the restored frequency wouldn't be the same as when the device wen't to sleep.
    Signed-off-by: franciscofranco <franciscofranco.1990@gmail.com>

--- arch/arm/kernel/mako_hotplug.c
+++ arch/arm/kernel/mako_hotplug.c
@@ -48,6 +48,7 @@ struct cpu_stats {
 	struct notifier_block notif;
 	u64 timestamp;
 	uint32_t freq;
+	uint32_t saved_freq;
 	bool screen_cap_lock;
 	bool suspend;
 } stats = {
@@ -264,7 +265,13 @@ static void screen_off_cap(bool nerf)
 {
 	int cpu;
 
-	stats.freq = nerf ? MAX_FREQ_CAP : LONG_MAX;
+	stats.freq = nerf ? MAX_FREQ_CAP : stats.saved_freq;
+
+	/*
+	 * This can be 0 on bootup if policy->max is not yet set
+	 */
+	if (!stats.freq)
+		stats.freq = LONG_MAX;
 
 	stats.screen_cap_lock = true;
 
@@ -276,7 +283,9 @@ static void screen_off_cap(bool nerf)
 
 static void mako_hotplug_suspend(struct work_struct *work)
 {
+	struct cpufreq_policy *policy = NULL;
 	int cpu;
+	int ret;
 
 	stats.counter = 0;
 
@@ -287,6 +296,17 @@ static void mako_hotplug_suspend(struct work_struct *work)
 		cpu_down(cpu);
 	}
 
+	/*
+	 * Save the current max freq before capping it to 1GHz
+	 * so that we can restore it after screen on.
+	 * Test for thermal throttle cases before merging on
+	 * production.
+	 */
+	ret = cpufreq_get_policy(policy, 0);
+	if (ret)
+		stats.saved_freq = LONG_MAX;
+	else
+		stats.saved_freq = policy->max;
 
 	stats.suspend = true;
 
