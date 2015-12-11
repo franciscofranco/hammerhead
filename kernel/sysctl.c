9e56b772bec5c5a85e49c1085d57d232fbff028a
franciscofranco <franciscofranco.1990@gmail.com>
2015-12-11 05:51:25 +0000

    kernel: sysctl: fix merge derp
    Signed-off-by: franciscofranco <franciscofranco.1990@gmail.com>

--- kernel/sysctl.c
+++ kernel/sysctl.c
@@ -327,7 +327,6 @@ static struct ctl_table kern_table[] = {
 		.mode		= 0644,
 		.proc_handler	= proc_dointvec,
 	},
-#ifdef CONFIG_SMP
 	{
 		.procname	= "sched_tunable_scaling",
 		.data		= &sysctl_sched_tunable_scaling,
