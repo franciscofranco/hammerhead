e829a2bb2fc726b818b23757c0a25c0b29d66076
franciscofranco <franciscofranco.1990@gmail.com>
2015-12-28 04:24:46 +0000

    sched/fair: Stop searching for tasks in newidle balance if there are runnable tasks
    It was found that when running some workloads (such as AIM7) on large
    systems with many cores, CPUs do not remain idle for long. Thus, tasks
    can wake/get enqueued while doing idle balancing.
    
    In this patch, while traversing the domains in idle balance, in
    addition to checking for pulled_task, we add an extra check for
    this_rq->nr_running for determining if we should stop searching for
    tasks to pull. If there are runnable tasks on this rq, then we will
    stop traversing the domains. This reduces the chance that idle balance
    delays a task from running.
    
    This patch resulted in approximately a 6% performance improvement when
    running a Java Server workload on an 8 socket machine.
    
    Signed-off-by: Jason Low <jason.low2@hp.com>
    Signed-off-by: Peter Zijlstra <peterz@infradead.org>
    Cc: Linus Torvalds <torvalds@linux-foundation.org>
    Cc: daniel.lezcano@linaro.org
    Cc: alex.shi@linaro.org
    Cc: preeti@linux.vnet.ibm.com
    Cc: efault@gmx.de
    Cc: vincent.guittot@linaro.org
    Cc: morten.rasmussen@arm.com
    Cc: aswin@hp.com
    Cc: chegu_vinod@hp.com
    Link: http://lkml.kernel.org/r/1398303035-18255-4-git-send-email-jason.low2@hp.com
    Signed-off-by: Ingo Molnar <mingo@kernel.org>
    Signed-off-by: franciscofranco <franciscofranco.1990@gmail.com>

--- kernel/sched/fair.c
+++ kernel/sched/fair.c
@@ -4930,7 +4930,6 @@ void idle_balance(int this_cpu, struct rq *this_rq)
 			continue;
 
 		if (sd->flags & SD_BALANCE_NEWIDLE) {
-			/* If we've pulled tasks over stop searching: */
 			pulled_task = load_balance(this_cpu, this_rq,
 						   sd, CPU_NEWLY_IDLE, &balance);
 		}
@@ -4938,7 +4937,12 @@ void idle_balance(int this_cpu, struct rq *this_rq)
 		interval = msecs_to_jiffies(sd->balance_interval);
 		if (time_after(next_balance, sd->last_balance + interval))
 			next_balance = sd->last_balance + interval;
-		if (pulled_task) {
+
+		/*
+		 * Stop searching for tasks to pull if there are
+		 * now runnable tasks on this rq.
+		 */
+		if (pulled_task || this_rq->nr_running > 0) {
 			this_rq->idle_stamp = 0;
 			break;
 		}
