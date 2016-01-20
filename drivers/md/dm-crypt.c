fd8b69fe50d976e6b5059a3d698e4c9c7cffc057
Tim Murray <timmurray@google.com>
2016-01-19 16:33:27 -0800

    dm-crypt: run in a WQ_HIGHPRI workqueue
    Running dm-crypt in a standard workqueue results in IO competing for CPU
    time with standard user apps, which can lead to pipeline bubbles and
    seriously degraded performance. Move to a WQ_HIGHPRI workqueue to
    protect against that.
    
    bug 25392275
    
    Change-Id: I589149a31c7b5d322fe2ed5b2476b1f6e3d5ee6f

--- drivers/md/dm-crypt.c
+++ drivers/md/dm-crypt.c
@@ -1641,6 +1641,7 @@ static int crypt_ctr(struct dm_target *ti, unsigned int argc, char **argv)
 
 	ret = -ENOMEM;
 	cc->io_queue = alloc_workqueue("kcryptd_io",
+				       WQ_HIGHPRI |
 				       WQ_NON_REENTRANT|
 				       WQ_MEM_RECLAIM,
 				       1);
@@ -1650,10 +1651,9 @@ static int crypt_ctr(struct dm_target *ti, unsigned int argc, char **argv)
 	}
 
 	cc->crypt_queue = alloc_workqueue("kcryptd",
-					  WQ_NON_REENTRANT|
-					  WQ_CPU_INTENSIVE|
-					  WQ_MEM_RECLAIM,
-					  1);
+					  WQ_HIGHPRI |
+					  WQ_MEM_RECLAIM |
+					  WQ_UNBOUND, num_online_cpus());
 	if (!cc->crypt_queue) {
 		ti->error = "Couldn't create kcryptd queue";
 		goto bad;
