db688798b6ebc6331bed1b0d3b65ab02bcdc71af
fluxi <linflux@arcor.de>
2016-05-05 12:14:39 +0200

    arm: arch_timer: Update for 64bit sched_clock
    Change-Id: Idf7e646ee4e5f4280ab087055a6ca96d0ed8eedb
    Signed-off-by: franciscofranco <franciscofranco.1990@gmail.com>

--- arch/arm/kernel/arch_timer.c
+++ arch/arm/kernel/arch_timer.c
@@ -304,12 +304,13 @@ static inline cycle_t counter_get_cntpct_mem(void)
 	return ((cycle_t) cvalh << 32) | cvall;
 }
 
-static inline cycle_t counter_get_cntpct_cp15(void)
+static inline u64 notrace counter_get_cntpct_cp15(void)
 {
-	u32 cvall, cvalh;
+	u64 cval;
 
-	asm volatile("mrrc p15, 0, %0, %1, c14" : "=r" (cvall), "=r" (cvalh));
-	return ((cycle_t) cvalh << 32) | cvall;
+	isb();
+	asm volatile("mrrc p15, 0, %Q0, %R0, c14" : "=r" (cval));
+	return cval;
 }
 
 static inline cycle_t counter_get_cntvct_mem(void)
@@ -360,23 +361,18 @@ static struct clocksource clocksource_counter = {
 	.flags	= CLOCK_SOURCE_IS_CONTINUOUS,
 };
 
-static u32 arch_counter_get_cntvct32(void)
+static inline u64 arch_counter_get_cntvct_cp15(void)
 {
-	cycle_t cntvct;
+	u64 cval;
 
-	cntvct = get_cntvct_func();
-
-	/*
-	 * The sched_clock infrastructure only knows about counters
-	 * with at most 32bits. Forget about the upper 24 bits for the
-	 * time being...
-	 */
-	return (u32)(cntvct & (u32)~0);
+	isb();
+	asm volatile("mrrc p15, 1, %Q0, %R0, c14" : "=r" (cval));
+	return cval;
 }
 
-static u32 notrace arch_timer_update_sched_clock(void)
+static u64 notrace arch_timer_update_sched_clock(void)
 {
-	return arch_counter_get_cntvct32();
+	return arch_counter_get_cntvct_cp15();
 }
 
 static void __cpuinit arch_timer_stop(struct clock_event_device *clk)
@@ -400,7 +396,7 @@ static void __init arch_timer_counter_init(void)
 {
 	clocksource_register_hz(&clocksource_counter, arch_timer_rate);
 
-	setup_sched_clock(arch_timer_update_sched_clock, 32, arch_timer_rate);
+	sched_clock_register(arch_timer_update_sched_clock, 56, arch_timer_rate);
 
 	/* Use the architected timer for the delay loop. */
 	arch_delay_timer.read_current_timer = &arch_timer_read_current_timer;
