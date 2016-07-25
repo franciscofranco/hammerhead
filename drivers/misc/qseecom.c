bbbd3211048bfcb966900548f18a4ac6c8457697
Mallikarjuna Reddy Amireddy <mamire@codeaurora.org>
2016-07-25 18:14:39 +0530

    qseecom: Change format specifier %p to %pK
    Format specifier %p can leak kernel addresses while not valuing the
    kptr_restrict system settings. When kptr_restrict is set to (1), kernel
    pointers printed using the %pK format specifier will be replaced with 0's.
    So that %pK will not leak kernel pointers to unprivileged users.
    So change the format specifier from %p to %pK.
    
    Debugging Note : &pK prints only Zeros as address. if you need actual
    address information, pls echo 0 to kptr_restrict.
    $ echo 0 > /proc/sys/kernel/kptr_restrict
    
    [Note for pick from bullhead:
     conflicts happened. I found %p manually, replaced it to %pK]
    
    Bug: 31498159
    Change-Id: I0baf2be2d5a476e2e4267f20b99d0ddf5492469e
    Signed-off-by: Mallikarjuna Reddy Amireddy <mamire@codeaurora.org>
    Signed-off-by: Francisco Franco <franciscofranco.1990@gmail.com>

--- drivers/misc/qseecom.c
+++ drivers/misc/qseecom.c
@@ -874,7 +874,7 @@ int __qseecom_process_rpmb_svc_cmd(struct qseecom_dev_handle *data_ptr,
 	void *req_buf = NULL;
 
 	if ((req_ptr == NULL) || (send_svc_ireq_ptr == NULL)) {
-		pr_err("Error with pointer: req_ptr = %p, send_svc_ptr = %p\n",
+		pr_err("Error with pointer: req_ptr = %pK, send_svc_ptr = %pK\n",
 			req_ptr, send_svc_ireq_ptr);
 		return -EINVAL;
 	}
@@ -1090,7 +1090,7 @@ static int qseecom_send_cmd(struct qseecom_dev_handle *data, void __user *argp)
 	if (ret)
 		return ret;
 
-	pr_debug("sending cmd_req->rsp size: %u, ptr: 0x%p\n",
+	pr_debug("sending cmd_req->rsp size: %u, ptr: 0x%pK\n",
 			req.resp_len, req.resp_buf);
 	return ret;
 }
@@ -1245,7 +1245,7 @@ static int qseecom_send_modfd_cmd(struct qseecom_dev_handle *data,
 	ret = __qseecom_update_cmd_buf(&req, true);
 	if (ret)
 		return ret;
-	pr_debug("sending cmd_req->rsp size: %u, ptr: 0x%p\n",
+	pr_debug("sending cmd_req->rsp size: %u, ptr: 0x%pK\n",
 			req.resp_len, req.resp_buf);
 	return ret;
 }
@@ -1784,7 +1784,7 @@ int qseecom_send_command(struct qseecom_handle *handle, void *send_buf,
 	if (ret)
 		return ret;
 
-	pr_debug("sending cmd_req->rsp size: %u, ptr: 0x%p\n",
+	pr_debug("sending cmd_req->rsp size: %u, ptr: 0x%pK\n",
 			req.resp_len, req.resp_buf);
 	return ret;
 }
