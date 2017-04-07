eb3c065b8b6d16d93c85586fff0b5ed2e19b5c91
Robert Baldyga <r.baldyga@samsung.com>
2017-04-07 11:01:11 -0700

    UPSTREAM: usb: gadget: f_fs: add "no_disconnect" mode
    Since we can compose gadgets from many functions, there is the problem
    related to gadget breakage while FunctionFS daemon being closed. FFS
    function is userspace code so there is no way to know when it will close
    files (it doesn't matter what is the reason of this situation, it can
    be daemon logic, program breakage, process kill or any other). So when
    we have another function in gadget which, for example, sends some amount
    of data, does some software update or implements some real-time functionality,
    we may want to keep the gadget connected despite FFS function is no longer
    functional.
    
    We can't just remove one of functions from gadget since it has been
    enumerated, so the only way to keep entire gadget working is to make
    broken FFS function deactivated but still visible to host. For this
    purpose this patch introduces "no_disconnect" mode. It can be enabled
    by setting mount option "no_disconnect=1", and results with defering
    function disconnect to the moment of reopen ep0 file or filesystem
    unmount. After closing all endpoint files, FunctionFS is set to state
    FFS_DEACTIVATED.
    
    When ffs->state == FFS_DEACTIVATED:
    - function is still bound and visible to host,
    - setup requests are automatically stalled,
    - transfers on other endpoints are refused,
    - epfiles, except ep0, are deleted from the filesystem,
    - opening ep0 causes the function to be closed, and then FunctionFS
      is ready for descriptors and string write,
    - altsetting change causes the function to be closed - we want to keep
      function alive until another functions are potentialy used, altsetting
      change means that another configuration is being selected or USB cable
      was unplugged, which indicates that we don't need to stay longer in
      FFS_DEACTIVATED state
    - unmounting of the FunctionFS instance causes the function to be closed.
    
    Tested-by: David Cohen <david.a.cohen@linux.intel.com>
    Acked-by: Michal Nazarewicz <mina86@mina86.com>
    Signed-off-by: Robert Baldyga <r.baldyga@samsung.com>
    Signed-off-by: Felipe Balbi <balbi@ti.com>
    
    Bug: 36801389
    Bug: 34873000
    Change-Id: I950dc11f21048c34af640cb3ab81873d2a6730a9
    Signed-off-by: Jerry Zhang <zhangjerry@google.com>
    Signed-off-by: D. Andrei Măceș <Andrei.Maces@alumni.nd.edu>
    Signed-off-by: Francisco Franco <franciscofranco.1990@gmail.com>

--- drivers/usb/gadget/f_fs.c
+++ drivers/usb/gadget/f_fs.c
@@ -22,6 +22,7 @@
 #include <linux/pagemap.h>
 #include <linux/export.h>
 #include <linux/hid.h>
+#include <linux/workqueue.h>
 #include <asm/unaligned.h>
 
 #include <linux/usb/composite.h>
@@ -71,6 +72,26 @@ enum ffs_state {
 	 * succeed.
 	 */
 	FFS_ACTIVE,
+
+	/*
+	 * Function is visible to host, but it's not functional. All
+	 * setup requests are stalled and transfers on another endpoints
+	 * are refused. All epfiles, except ep0, are deleted so there
+	 * is no way to perform any operations on them.
+	 *
+	 * This state is set after closing all functionfs files, when
+	 * mount parameter "no_disconnect=1" has been set. Function will
+	 * remain in deactivated state until filesystem is umounted or
+	 * ep0 is opened again. In the second case functionfs state will
+	 * be reset, and it will be ready for descriptors and strings
+	 * writing.
+	 *
+	 * This is useful only when functionfs is composed to gadget
+	 * with another function which can perform some critical
+	 * operations, and it's strongly desired to have this operations
+	 * completed, even after functionfs files closure.
+	 */
+	FFS_DEACTIVATED,
 
 	/*
 	 * All endpoints have been closed.  This state is also set if
@@ -233,6 +254,9 @@ struct ffs_data {
 		uid_t				uid;
 		gid_t				gid;
 	}				file_perms;
+
+	bool no_disconnect;
+	struct work_struct reset_work;
 
 	/*
 	 * The endpoint files, filled by ffs_epfiles_create(),
@@ -1096,6 +1120,7 @@ struct ffs_sb_fill_data {
 	struct ffs_file_perms perms;
 	umode_t root_mode;
 	const char *dev_name;
+	bool no_disconnect;
 	struct ffs_data *ffs_data;
 };
 
@@ -1167,6 +1192,12 @@ static int ffs_fs_parse_opts(struct ffs_sb_fill_data *data, char *opts)
 
 		/* Interpret option */
 		switch (eq - opts) {
+		case 13:
+			if (!memcmp(opts, "no_disconnect", 13))
+				data->no_disconnect = !!value;
+			else
+				goto invalid;
+			break;
 		case 5:
 			if (!memcmp(opts, "rmode", 5))
 				data->root_mode  = (value & 0555) | S_IFDIR;
@@ -1222,6 +1253,7 @@ ffs_fs_mount(struct file_system_type *t, int flags,
 			.gid = 0
 		},
 		.root_mode = S_IFDIR | 0500,
+		.no_disconnect = false,
 	};
 	struct dentry *rv;
 	int ret;
@@ -1238,6 +1270,7 @@ ffs_fs_mount(struct file_system_type *t, int flags,
 	if (unlikely(!ffs))
 		return ERR_PTR(-ENOMEM);
 	ffs->file_perms = data.perms;
+	ffs->no_disconnect = data.no_disconnect;
 
 	ffs->dev_name = kstrdup(dev_name, GFP_KERNEL);
 	if (unlikely(!ffs->dev_name)) {
@@ -1269,6 +1302,7 @@ ffs_fs_kill_sb(struct super_block *sb)
 	kill_litter_super(sb);
 	if (sb->s_fs_info) {
 		functionfs_release_dev_callback(sb->s_fs_info);
+		ffs_data_closed(sb->s_fs_info);
 		ffs_data_put(sb->s_fs_info);
 	}
 }
@@ -1324,7 +1358,11 @@ static void ffs_data_opened(struct ffs_data *ffs)
 	ENTER();
 
 	atomic_inc(&ffs->ref);
-	atomic_inc(&ffs->opened);
+	if (atomic_add_return(1, &ffs->opened) == 1 &&
+			ffs->state == FFS_DEACTIVATED) {
+		ffs->state = FFS_CLOSING;
+		ffs_data_reset(ffs);
+	}
 }
 
 static void ffs_data_put(struct ffs_data *ffs)
@@ -1346,6 +1384,21 @@ static void ffs_data_closed(struct ffs_data *ffs)
 	ENTER();
 
 	if (atomic_dec_and_test(&ffs->opened)) {
+		if (ffs->no_disconnect) {
+			ffs->state = FFS_DEACTIVATED;
+			if (ffs->epfiles) {
+				ffs_epfiles_destroy(ffs->epfiles,
+						   ffs->eps_count);
+				ffs->epfiles = NULL;
+			}
+			if (ffs->setup_state == FFS_SETUP_PENDING)
+				__ffs_ep0_stall(ffs);
+		} else {
+			ffs->state = FFS_CLOSING;
+			ffs_data_reset(ffs);
+		}
+	}
+	if (atomic_read(&ffs->opened) < 0) {
 		ffs->state = FFS_CLOSING;
 		ffs_data_reset(ffs);
 	}
@@ -1605,10 +1658,12 @@ static void ffs_func_eps_disable(struct ffs_function *func)
 		/* pending requests get nuked */
 		if (likely(ep->ep))
 			usb_ep_disable(ep->ep);
-		epfile->ep = NULL;
-
 		++ep;
-		++epfile;
+
+		if (epfile) {
+			epfile->ep = NULL;
+			++epfile;
+		}
 	} while (--count);
 	spin_unlock_irqrestore(&func->ffs->eps_lock, flags);
 }
@@ -2436,6 +2491,13 @@ error:
 
 /* Other USB function hooks *************************************************/
 
+static void ffs_reset_work(struct work_struct *work)
+{
+	struct ffs_data *ffs = container_of(work,
+		struct ffs_data, reset_work);
+	ffs_data_reset(ffs);
+}
+
 static void ffs_func_unbind(struct usb_configuration *c,
 			    struct usb_function *f)
 {
@@ -2470,6 +2532,13 @@ static int ffs_func_set_alt(struct usb_function *f,
 	if (ffs->func) {
 		ffs_func_eps_disable(ffs->func);
 		ffs->func = NULL;
+	}
+
+	if (ffs->state == FFS_DEACTIVATED) {
+		ffs->state = FFS_CLOSING;
+		INIT_WORK(&ffs->reset_work, ffs_reset_work);
+		schedule_work(&ffs->reset_work);
+		return -ENODEV;
 	}
 
 	if (ffs->state != FFS_ACTIVE)
