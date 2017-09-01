a886a875b90eebe1204895b15f0058fcabd83b3e
Francisco Franco <franciscofranco.1990@gmail.com>
2017-09-01 15:27:28 +0000

    cpufreq: cpu-boost: don't raise min freq if the current frequency is already >= boost frequency
    Signed-off-by: Francisco Franco <franciscofranco.1990@gmail.com>

--- drivers/cpufreq/cpu-boost.c
+++ drivers/cpufreq/cpu-boost.c
@@ -158,6 +158,9 @@ static int boost_adjust_notify(struct notifier_block *nb, unsigned long val,
 		min = max(b_min, ib_min);
 		min = min(min, policy->max);
 
+		if (policy->cur >= ib_min)
+			break;
+
 		pr_debug("CPU%u policy min before boost: %u kHz\n",
 			 cpu, policy->min);
 		pr_debug("CPU%u boost min: %u kHz\n", cpu, min);
